1d2a5e92820dd84e18f4f511a11f15ba65d639f8
Tsugoii <109778420+tsugoii@users.noreply.github.com>
2025-04-07 12:19:34 -0600

    Update solution.java
    Forgot some stuff xd

--- solution.java
+++ solution.java
@@ -1,26 +1,42 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        // Will handle case where values are negative
+        // If there aren't enough values for a triplet
+        int n = nums.length;
+        if (n < 3) {
+            return 0;
+        }
+        // Max Value, Max Value of left of index and right of index
         long maxVal = 0;
-        // For each possible middle index
-        // j has to be at least 1 and at most n - 2 for i<j<k
-        for (int j = 1; j < nums.length - 1; j++) {
-            int maxLeft = 0;
-            int maxRight = 0;
-            // Iterate through cases where i < j
-            for (int i = 0; i < j; i++) {
-                // Update max if current is greater
-                maxLeft = Math.max(maxLeft, nums[i]);
-            }
-            // Iterate through j < k
-            for (int k = j + 1; k < nums.length; k++) {
-                // update max if current is g reater
-                maxRight = Math.max(maxRight, nums[k]);
-            }
-            // calculate (i, j, k) using maxRight and maxLeft and current j
-            long currentValue  = (long) (maxLeft - nums[j]) * maxRight;
+        // [max 0... i-1]
+        int[] maxLeft = new int[n];
+        // max of left is going to be at 0 index, since middle has to start at index 1 
+        maxLeft[0] = 0;
+        // [max i+1... n-1]
+        int[] maxRight = new int[n];
+        // max of right is going to be middle - 1
+        maxRight[n - 1] = 0;
+
+        // From second element to the end
+        for (int i = 1; i < n; i++) {
+            maxLeft[i] = Math.max(maxLeft[i - 1], nums[i - 1]);
+        }
+        System.out.println("maxLeft: " + java.util.Arrays.toString(maxLeft));
+
+        for (int i = n - 2; i >= 0; i--) {
+            maxRight[i] = Math.max(maxRight[i + 1], nums[i + 1]);
+        }
+        System.out.println("maxRight: " + java.util.Arrays.toString(maxRight));
+        // From all possible middle indicies
+        for (int j = 1; j < n  - 1; j++) {
+            // For current idex j, maxLeft[j] is max of nums[i] for i < j
+            // maxRight[j] holds max of nums[k] where j < k
+            // Then calculates value of triplet throuh i - j * k
+            long currentValue = (long) ((maxLeft[j] - nums[j]) * maxRight[j]);
+
+            // If current value is greater than max, update
             maxVal = Math.max(maxVal, currentValue);
         }
+        // Retun maximum triplet value found
         return maxVal;
     }
 }
